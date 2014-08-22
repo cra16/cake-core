ec18d15fc886e18d12190506b00a0eec9bee3f00
Joshua Jung <wbqdinno@gmail.com>
2014-08-22 13:11:42 +0900

    출력 콘솔 추가, 각 요소에 panel 클래스 추가

--- dev/js/code.js
+++ dev/js/code.js
@@ -28,3 +28,19 @@ function renderContent() {
     content.innerHTML = code;
   };
 }
+
+jQuery(function($, undefined) {
+    $('#terminal').terminal(function(command, term) {
+        if (command !== '') {
+            var result = window.eval(command);
+            if (result != undefined) {
+                term.echo(String(result));
+            }
+        }
+    }, {
+        greetings: 'Cake Console Termnal',
+        name: 'js_demo',
+        height: 100,
+        width: 600,
+        prompt: 'cake> '});
+});
